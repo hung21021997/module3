98c2153bb46e8b2f54d0d90f55b47d4eb4039cfd
Hung21021612 <thaivanhung12a3@gmail.com>
2020-07-13 13:31:04 +0700

    up

---
+++ Angular/Thuc_hanh/angular-overview/src/app/name-card/name-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-name-card',
+  templateUrl: './name-card.component.html',
+  styleUrls: ['./name-card.component.scss']
+})
+export class NameCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Angular/Thuc_hanh/angular-overview/src/app/rating-bar/rating-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rating-bar',
+  templateUrl: './rating-bar.component.html',
+  styleUrls: ['./rating-bar.component.scss']
+})
+export class RatingBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
